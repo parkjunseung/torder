c60c0722d3cab3e4436b1667a1c51f8c63732c7c
JunseungPark <87506155+JunseungPark@users.noreply.github.com>
2022-03-26 21:16:57 +0900

    router이름 변경

--- src/router/index.js
+++ src/router/index.js
@@ -9,17 +9,17 @@ const routes = [
   {
     path: '/main',
     name: 'main',
-    component: () => import('@/views/main'),
+    component: () => import('@/views/Main'),
   },
   {
     path: '/orderHistory',
     name: 'orderHistory',
-    component: () => import('@/views/orderHistory'),
+    component: () => import('@/views/OrderHistory'),
   },
   {
     path: '/bill',
     name: 'bill',
-    component: () => import('@/views/bill'),
+    component: () => import('@/views/Bill'),
   },
 ];
 
